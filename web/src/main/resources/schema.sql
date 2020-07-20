6e4817793f3b12faf183af72150c067383fb902b
Naz <naz_eze@yahoo.co.uk>
2020-07-20 20:51:07 +0200

    Add DB schema

---
+++ web/src/main/resources/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS agents (
+    id INT(20) NOT NULL AUTO_INCREMENT,
+    created_time TIMESTAMP DEFAULT now(),
+    updated_time TIMESTAMP DEFAULT now() ON UPDATE now(),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255),
+    phone_number VARCHAR(255),
+    device_id VARCHAR(255),
+    is_email_verified BIT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS leads (
+    id INT(20) NOT NULL AUTO_INCREMENT,
+    created_time TIMESTAMP DEFAULT now(),
+    updated_time TIMESTAMP DEFAULT now() ON UPDATE now(),
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(255) NOT NULL,
+    post_code VARCHAR(255) NOT NULL,
+    lead_status VARCHAR(255) NOT NULL,
+    lead_type VARCHAR(255) NOT NULL,
+    motivation VARCHAR(255),
+    property_type VARCHAR(255),
+    years_in_property VARCHAR(255),
+    agent_id INT(20),
+    PRIMARY KEY (id),
+    FOREIGN KEY (agent_id) REFERENCES agents (id)
+);
