44b328708e80fedfc754ace1ef52a02beda496ea
Naz <naz_eze@yahoo.co.uk>
2020-08-03 09:10:46 +0100

    Adds sorting to comments returned from the database

--- web/src/main/java/com/restateai/repository/CommentRepository.java
+++ web/src/main/java/com/restateai/repository/CommentRepository.java
@@ -1,8 +1,13 @@
 package com.restateai.repository;
 
 import com.restateai.model.lead.Comment;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends JpaRepository <Comment, Long> {
 
+    @Override
+    @Query(value = "SELECT * FROM comments ORDER BY created_time DESC;", nativeQuery = true)
+    List<Comment> findAll();
 }
