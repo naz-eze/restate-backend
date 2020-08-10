1ac2fe25b82b9cc026e4ea19e5e5e53c83daa6df
Naz <naz_eze@yahoo.co.uk>
2020-08-10 21:51:21 +0100

    Refactor - renamed RestateApplication to Application

--- web/src/main/java/com/restateai/Application.java
+++ web/src/main/java/com/restateai/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestateApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestateApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- web/src/test/java/com/restateai/ApplicationTests.java
+++ web/src/test/java/com/restateai/ApplicationTests.java
@@ -3,8 +3,7 @@ package com.restateai;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class RestateApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
